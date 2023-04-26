f844d63396187b7ab97216694f76f5fb1b178947
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-04-26 18:43:11 +0000

    Created a card and an option to randomize it

--- src/app.js
+++ src/app.js
@@ -7,5 +7,48 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let all_suits = ["♦", "♥", "♠", "♣"];
+  let all_numbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  change_suit(all_suits, all_numbers);
+
+  //change button
+  document.querySelector(".change_suit").addEventListener("click", () => {
+    change_suit(all_suits, all_numbers);
+  });
+};
+
+const change_suit = (all_suits, all_numbers) => {
+  //Chosing suit
+  let suit = document.querySelectorAll(".suit");
+  const random_suit = Math.floor(Math.random() * all_suits.length);
+  suit.forEach(x => {
+    x.innerHTML = all_suits[random_suit];
+    if (all_suits[random_suit] == "♥" || all_suits[random_suit] == "♦") {
+      x.style.color = "red";
+    } else if (all_suits[random_suit] == "♣" || all_suits[random_suit] == "♠") {
+      x.style.color = "black";
+    }
+  });
+  //end of chosing suit
+  //chosing number
+  let number = document.querySelector(".number");
+  const random_number = Math.floor(Math.random() * all_numbers.length);
+  number.innerHTML = all_numbers[random_number];
+  //end of chosing number
 };
